e2b733d059160f645819113d12a132e58c1b24c4
Style <esrsamarth@gmail.com>
2024-12-08 01:42:11 +0530

    added all languages

--- src/i18n.js
+++ src/i18n.js
@@ -1,4 +1,3 @@
-
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import Backend from 'i18next-http-backend';
@@ -7,6 +6,7 @@ i18n
   .use(Backend)
   .use(initReactI18next)
   .init({
+    supportedLngs: ['en', 'kn', 'te', 'ta', 'hi'], // Make sure 'ta' is included
     fallbackLng: 'en',
     debug: true,
     backend: {

a235f499c70d97c1703d6fe4cbb4437058562c0e
Style <esrsamarth@gmail.com>
2024-12-08 05:40:34 +0530

    fixed the images not appearing bug on host

--- src/data/schemeDetails/pmfby.js
+++ src/data/schemeDetails/pmfby.js
@@ -11,102 +11,102 @@ export const pmfby = {
     {
       kannada: "pmfby.gov.in ಅಧಿಕೃತ ಪೋರ್ಟಲ್‌ಗೆ ಭೇಟಿ ನೀಡಿ",
       english: "Go to the official portal at pmfby.gov.in",
-      image: "/public/fasalBhima/1.jpg"
+      image: "/fasalBhima/1.jpg"
     },
     {
       kannada: "ಬೆಳೆ ವಿಮೆಗಾಗಿ ಅರ್ಜಿ ಸಲ್ಲಿಸಲು ರೈತರ ಕಾರ್ನರ್ ಇರುತ್ತದೆ",
       english: "There will be a farmer corner where we can apply for crop insurance",
-      image: "/public/fasalBhima/2.jpg"
+      image: "/fasalBhima/2.jpg"
     },
     {
       kannada: "ರೈತರ ಕಾರ್ನರ್ ಕ್ಲಿಕ್ ಮಾಡಿದಾಗ ಲಾಗಿನ್ ಪುಟ ಸಿಗುತ್ತದೆ",
       english: "When you click on farmer corner, you will get a login page",
-      image: "/public/fasalBhima/3.jpg"
+      image: "/fasalBhima/3.jpg"
     },
     {
       kannada: "ನೋಂದಣಿ ಆಯ್ಕೆಯನ್ನು ಕ್ಲಿಕ್ ಮಾಡಿ",
       english: "Click on the enrollment option",
-      image: "/public/fasalBhima/4.jpg"
+      image: "/fasalBhima/4.jpg"
     },
     {
       kannada: "CSC ಕ್ಲಿಕ್ ಮಾಡಿ",
       english: "Now click on CSC",
-      image: "/public/fasalBhima/5.jpg"
+      image: "/fasalBhima/5.jpg"
     },
     {
       kannada: "ಇ-ಮೇಲ್, ಪಾಸ್‌ವರ್ಡ್ ಮತ್ತು ಕ್ಯಾಪ್ಚಾ ನಮೂದಿಸಿ ಲಾಗಿನ್ ಆಗಿ",
       english: "Enter E-mail, password and captcha to login",
-      image: "/public/fasalBhima/6.jpg"
+      image: "/fasalBhima/6.jpg"
     },
     {
       kann나다: "ರಾಜ್ಯ ಮತ್ತು ಯೋಜನೆಯನ್ನು ಆಯ್ಕೆ ಮಾಡಿ ಮತ್ತು ಸಲ್ಲಿಸಿ",
       english: "Select the state and the Yojana you are applying to and click submit",
-      image: "/public/fasalBhima/7.jpg"
+      image: "/fasalBhima/7.jpg"
     },
     {
       kannada: "ಅರ್ಜಿ ಆಯ್ಕೆಗೆ ಹೋಗಿ",
       english: "Move to the Application option",
-      image: "/public/fasalBhima/8.jpg"
+      image: "/fasalBhima/8.jpg"
     },
     {
       kannada: "CSC ಅರ್ಜಿಗಳಲ್ಲಿ ಅರ್ಜಿ ನಮೂನೆಯನ್ನು ಕ್ಲಿಕ್ ಮಾಡಿ",
       english: "Click on Application form in CSC applications",
-      image: "/public/fasalBhima/9.jpg"
+      image: "/fasalBhima/9.jpg"
     },
     {
       kannada: "ರೈತ ಅರ್ಜಿ ನಮೂನೆ ಭರ್ತಿ ಮಾಡಿ ಮತ್ತು ಬ್ಯಾಂಕ್ ಬ್ಯಾಲೆನ್ಸ್ ಪರಿಶೀಲಿಸಿ",
       english: "Fill the Farmer application form and click on check bank balance and continue",
-      image: "/public/fasalBhima/10.jpg"
+      image: "/fasalBhima/10.jpg"
     },
     {
       kannada: "ವಾಸಸ್ಥಳದ ವಿಳಾಸವನ್ನು ಭರ್ತಿ ಮಾಡಿ",
       english: "Fill in the Residential address",
-      image: "/public/fasalBhima/11.jpg"
+      image: "/fasalBhima/11.jpg"
     },
     {
       kannada: "ನಾಮಿನಿ ವಿವರಗಳನ್ನು ಭರ್ತಿ ಮಾಡಿ ಮತ್ತು ಮುಂದುವರಿಸಿ",
       english: "Fill Nominee details and click on save and continue",
-      image: "/public/fasalBhima/12.jpg"
+      image: "/fasalBhima/12.jpg"
     },
     {
       kannada: "ಫಾರ್ಮ್ ಭರ್ತಿ ಮಾಡುವ ಮುಂದಿನ ಪ್ರಕ್ರಿಯೆ",
       english: "This is further process in filling the form",
-      image: "/public/fasalBhima/13.jpg"
+      image: "/fasalBhima/13.jpg"
     },
     {
       kannada: "ಬೆಳೆಗಳ ಮಾಲೀಕತ್ವ ಪರಿಶೀಲನೆ",
       english: "Verify if you are owner of the crops",
-      image: "/public/fasalBhima/14.jpg"
+      image: "/fasalBhima/14.jpg"
     },
     {
       kannada: "ಮಾಲೀಕರ ಬೆಳೆ ವಿವರಗಳನ್ನು ನೀಡಲಾ���ುತ್ತದೆ",
       english: "Details of the owners crop will be provided",
-      image: "/public/fasalBhima/15.jpg"
+      image: "/fasalBhima/15.jpg"
     },
     {
       kannada: "ರೈತರ ಭೂಮಿ ವಿವರಗಳು",
       english: "Farmers land details",
-      image: "/public/fasalBhima/16.jpg"
+      image: "/fasalBhima/16.jpg"
     },
     {
       kannada: "ಅಗತ್ಯ ದಾಖಲೆಗಳನ್ನು ಅಪ್‌ಲೋಡ್ ಮಾಡಿ ಮತ್ತು ಪೂರ್ವವೀಕ್ಷಣೆ ಕ್ಲಿಕ್ ಮಾಡಿ",
       english: "Upload the required documents and click on preview",
-      image: "/public/fasalBhima/17.jpg"
+      image: "/fasalBhima/17.jpg"
     },
     {
       kannada: "6 ಅಂಕಿಯ ವಾಲೆಟ್ ಸಂಖ್ಯೆ ನಮೂದಿಸಿ ಮತ್ತು ಪಾವತಿಸಿ",
       english: "Enter 6 digit wallet number and pay the required amount",
-      image: "/public/fasalBhima/18.jpg"
+      image: "/fasalBhima/18.jpg"
     },
     {
       kannada: "ಪಾವತಿ ಯಶಸ್ವಿಯಾದ ನಂತರ ಸಂದೇಶ ಸಿಗುತ್ತದೆ",
       english: "Once the payment is done you will get the message of Payment success",
-      image: "/public/fasalBhima/19.jpg"
+      image: "/fasalBhima/19.jpg"
     },
     {
       kannada: "ಪಾವತಿ ಯಶಸ್ವಿಯಾದ ನಂತರ ಪ್ರಿಂಟ್ ಆಯ್ಕೆ ಕ್ಲಿಕ್ ಮಾಡಿ ಅರ್ಜಿ ಪಡೆಯಿರಿ",
       english: "After successful payment, click on print option to get your application",
-      image: "/public/fasalBhima/20.jpg"
+      image: "/fasalBhima/20.jpg"
     }
   ]
 };
